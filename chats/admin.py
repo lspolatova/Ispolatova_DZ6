7406b5e73b51cbd73b05c4fb4fed366063f004cd
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-12-06 20:39:52 +0300

    Delete chats directory

--- chats/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from chats.models import Chat, Category, Files, Messages
-admin.site.register(Chat)
-admin.site.register(Category)
-admin.site.register(Files)
-admin.site.register(Messages)
